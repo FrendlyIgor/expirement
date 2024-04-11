34c8f258ada0a1d64d197ced513511106980a652
FrendlyIgor <grafdruzhin@gmail.com>
2024-04-11 11:35:04 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 const swiper = new Swiper('.top__swiper', {
     // Optional parameters
     effect: "fade",
-    autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
-    },
+    // autoplay: {
+    //   delay: 3500,
+    //   disableOnInteraction: false,
+    // },
   
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
