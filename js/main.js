89420d45d98078f736e479732dfe16cb074ab7f7
FrendlyIgor <grafdruzhin@gmail.com>
2024-04-14 23:40:15 +0300

    final change

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const swiper = new Swiper('.top__swiper', {
-    // Optional parameters
     effect: "fade",
-    // autoplay: {
-    //   delay: 3500,
-    //   disableOnInteraction: false,
-    // },
-  
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
